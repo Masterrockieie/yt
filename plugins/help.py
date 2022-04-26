8067b879de6f0a2e742ea52c5b743957c3df1daa
Amal nath <96761219+Masterrockiei@users.noreply.github.com>
2022-04-26 21:22:57 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -3,5 +3,5 @@ from pyrogram import Client, Filters
 
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
-    helptxt =<a href=https://t.me/kerala_status_downloader>Just send the link to the group</a>"
+    helptxt = Just send the link to the group
     await message.reply_text(helptxt)
